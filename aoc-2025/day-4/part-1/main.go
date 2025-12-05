1e9d1b03efcb78fc70411cc54659bb70d9675d61
Mushfiq Mahmud <mushfiq8194@gmail.com>
2025-12-05 00:01:47 -0500

    2025: complete d4p1

--- aoc-2025/day-4/part-1/main.go
+++ aoc-2025/day-4/part-1/main.go
@@ -1,16 +1,54 @@
 package d4p1
 
-import (
-	"fmt"
-)
+type GridSize struct {
+	Rows int
+	Cols int
+}
 
-func TotalScore(lines []string) int {
-	score := 0
-	fmt.Println("TODO")
+type Position struct {
+	i int
+	j int
+}
 
-	for _, line := range lines {
-		fmt.Println(line)
+var neighborOffsets = []Position{
+	{-1, -1}, {-1, 0}, {-1, 1},
+	{0, -1}, {0, 1},
+	{1, -1}, {1, 0}, {1, 1},
+}
+
+func TotalScore(grid []string) int {
+	if len(grid) == 0 || len(grid[0]) == 0 {
+		return 0
 	}
 
+	score := 0
+	gs := GridSize{Rows: len(grid), Cols: len(grid[0])}
+
+	for i := range grid {
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] != '@' {
+				continue
+			}
+
+			count := 0
+			for _, offset := range neighborOffsets {
+				neighborPos := Position{i: i + offset.i, j: j + offset.j}
+				if CheckIfRoll(grid, gs, neighborPos) {
+					count++
+				}
+			}
+
+			if count < 4 {
+				score++
+			}
+		}
+	}
 	return score
+}
+
+func CheckIfRoll(grid []string, grid_size GridSize, pos Position) bool {
+	if pos.i < 0 || pos.j < 0 || pos.i >= grid_size.Rows || pos.j >= grid_size.Cols {
+		return false
+	}
+	return grid[pos.i][pos.j] == '@'
 }
